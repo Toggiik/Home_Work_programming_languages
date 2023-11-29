59421d4252c03224e3a1ca32af7b7171a675fc40
tolyan <arg-r@mail.ru>
2023-11-29 22:45:18 +0300

    Решили вторую задачу

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿namespace Task_01 {
+public class Task_02 {
+		public static void Main (string [] arg){
+			int size = 10;	
+			int [] array = new int [size];
+			Random rand = new Random(); 
+			int count = 0;
+			for (int i = 0; i < size; i++ ){ 
+				array[i] = rand.Next(0,20);
+			}
+			foreach (int item in array){ 
+				System.Console.Write (item + " ");
+			}
+            System.Console.WriteLine();
+			for (int i = 0; i < size; i++ ){ 
+				if (array [i]%2==0) count++; 
+			}
+			System.Console.Write (count);
+		}
+	}
+}    
